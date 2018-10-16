e4441030882317523079005aea83d2b873c6ef17
mtldswz <962705972@qq.com>
2018-10-16 15:53:20 +0800

    Update Point.py

--- ModeInter/Point.py
+++ ModeInter/Point.py
@@ -1,15 +1,27 @@
+#!/usr/bin/env python
 # -*-coding: utf-8-*-
 
+
 class Point(object):
-
     def __init__(self, x, y, z):
-
+        super(Point, self).__init__()
         self.x = x
         self.y = y
         self.z = z
+
+    def write_data(self, fp=None):
+        self.x = float(self.x)
+        self.y = float(self.y)
+        self.z = float(self.z)
+        print("%15.7E %15.7E %15.7E" % (self.x, self.y, self.z), file=fp)
 
 
 class ModeShape(Point):
 
     def __init__(self, x, y, z):
         super(ModeShape, self).__init__(x, y, z)
+
+
+if __name__ == "__main__":
+    point = Point(1.0, 2.0, 3.0)
+    point.write_data()
