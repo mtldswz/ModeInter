2cc6f981b439c40c52bc68fed427bfc8ce7fe583
mtldswz <962705972@qq.com>
2018-10-16 17:29:55 +0800

    Update main.sh

--- ModeInter/main.sh
+++ ModeInter/main.sh
@@ -7,3 +7,19 @@ curPath=`pwd`
 python main.py
 
 retcode=$?
+
+
+# -*- coding:utf-8 -*-
+
+import CFDMesh
+import InterWorkr
+import Get3DModeShape
+import Point
+
+if __name__ == "__main__":
+    cfl3dmesh = CFDMesh.Plot3DMesh()
+    cfl3dmesh.run()
+    rbfworker = InterWorkr.MockRBFMethod()
+    n_mode = rbfworker.run()
+    get3d = Get3DModeShape(n_mode)
+    get3d.run()
