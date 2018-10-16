a991cd91b9edac89c1a0cbfac2532792c2b45bae
mtldswz <962705972@qq.com>
2018-10-16 15:51:33 +0800

    Update InterWorker.py

--- ModeInter/InterWorker.py
+++ ModeInter/InterWorker.py
@@ -2,6 +2,8 @@
 
 import CSDMesh
 import CFDMesh
+import subprocess
+import os
 
 
 class InterWork(object):
@@ -11,19 +13,60 @@ class InterWork(object):
         self.list_modeshape = []
 
     def run(self):
+        raise Exception("Abstract method.")
 
-        for point in self.cfdmesh.listpoint:
-            modeshape = self._run_one_point(point)
-            self.list_modeshape.append(modeshape)
 
-    def _run_one_point(self, point):
+class MockRBFMethod(InterWork):
 
-        list_modeshape = []
+    def __init__(self, csdmesh="mode.dat", cfdmesh="wg.dat", inp="", filehead=""):
+        super(MockRBFMethod, self).__init__(csdmesh, cfdmesh)
+        self.inputfile = inp
+        self.filehead = filehead
+        self.codef90 = "MODULM.f90 RBF_MODE.f90"
+        self.worker = "RBF_MODE.o"
+        self.compiler = None
 
-        for imode in range(self.csdmesh.n_mode):
-            self.list_modeshape.append()
+    def check_file(self):
+        if not os.path.isfile("MODULM.f90") or not os.path.isfile("RBF_MODE.f90"):
+            raise Exception("file %s does't exist!" % self.codef90)
+        if not os.path.isfile(self.csdmesh):
+            raise Exception("file %s does't exist!" % self.csdmesh)
+        if not os.path.isfile(self.cfdmesh):
+            raise Exception("file %s does't exist!" % self.cfdmesh)
+        if not os.path.isfile(self.inputfile):
+            raise Exception("file %s does't exist!" % self.inputfile)
+        if not os.path.isfile(self.filehead):
+            raise Exception("file %s does't exist!" % self.filehead)
+        return True
 
-        return list_modeshape
+    def check_sys(self):
+        cmd = "which ifort"
+        rc = subprocess.call(cmd, shell=True)
+        if rc == 0:
+            self.compiler = "ifort"
+            return True
+        cmd = "which gfortran"
+        rc = subprocess.call(cmd, shell=True)
+        if rc == 0:
+            self.compiler = "gfortran"
+            return True
+        raise Exception("no Frotran compiler! Pleas install 'ifort' or 'gfortran'")
 
-    def output_inter_res(self):
-        pass
+    def compile_f90(self):
+        if self.compiler is None:
+            raise Exception("no Frotran compiler! Pleas install 'ifort' or 'gfortran'")
+        cmd = self.compiler + self.codef90 + "-o" + self.worker
+        rc = subprocess.call(cmd, shell=True)
+        if rc != 0:
+            raise Exception("compile f90 code fail!")
+
+    def run(self):
+        if self.check_sys() and self.check_file():
+            self.compile_f90()
+            cmd = os.path.join(".", self.worker)
+            rc.subprocess.call(cmd, shell=True)
+
+
+if __name__ == "__main__":
+    nrbf = MockRBFMethod()
+    nrbf.run()
